b9d3628c102168e87671d0f5e1e9d26073c2023e
yairmi <7098597+yairmi@users.noreply.github.com>
2019-11-22 21:26:58 +0200

    new message
    added adi and noa

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Hello from Yair!</h2>
+      <h2>Hello from Yair and Adi and Noa!</h2>
     </div>
   );
 }
